35e5973cbf59418145e3c3f14f27d18b4c508c78
gongzhilong <102662525+gongzhilong@users.noreply.github.com>
2022-04-21 18:44:15 +0800

    Update list.c

--- list.c
+++ list.c
@@ -10,3 +10,19 @@ struct list *create_list()
     l->counter = 0; 
     return l;	
 }
+struct list *insert( struct list *l, int i, T e ) {	
+	int j;
+
+    if( l->count == MAX_ELE ) return NULL;
+	assert( i >= 0 && i <= l->count );
+
+	for( j=l->count; j>i; j-- ) {
+		l->ele[j] = l->ele[j-1];
+	}
+
+	l->count ++;
+	
+	l->ele[i] = e;
+	
+	return l;
+}
